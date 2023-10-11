070cca1acf2dc7708afcbfef02bde9bad5279886
eQu33nox <equueenox@gmail.com>
2023-10-11 13:27:04 +0600

    filter and newCards

--- src/components/cards/CardsFilter/CardsFilter.jsx
+++ src/components/cards/CardsFilter/CardsFilter.jsx
@@ -1,36 +1,35 @@
-// import React, { useEffect } from "react";
-// import { getCards, getCatygories } from "../../../store/cards/cardsActions";
-// import { changeCategory } from "../../../store/cards/cardsSlice";
-// import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { getCards, getCategories } from "../../../store/cards/cardsActions";
+import { changeCategory } from "../../../store/cards/cardsSlice";
+import { useDispatch, useSelector } from "react-redux";
 
-// const CardsFilter = () => {
-//   const { categories } = useSelector((state) => state.cards);
-//   const dispatch = useDispatch();
+const CardsFilter = () => {
+  const { categories } = useSelector((state) => state.cards);
+  const dispatch = useDispatch();
 
-//   useEffect(() => {
-//     dispatch(getCatygories());
-//   }, []);
+  useEffect(() => {
+    dispatch(getCategories());
+  }, []);
 
-//   return (
-//     <div>
-//       <div className="w-1/6">
-//         <select
-//           onChange={(e) => {
-//             dispatch(changeCategory({ category: e.target.value }));
-//             dispatch(getCards());
-//           }}
-//           className="mt-1.5 w-full h-9 rounded-lg bg-pink-500 text-white sm:text-sm"
-//         >
-//           <option value="all">all</option>
-//           {categories.map((category) => (
-//             <option key={category} value={category}>
-//               {category}
-//             </option>
-//           ))}
-//         </select>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <div className="w-1/6">
+        <select
+          onChange={(e) => {
+            dispatch(changeCategory({ category: e.target.value }));
+            dispatch(getCards());
+          }}
+        >
+          <option value="all">all</option>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
 
-// export default CardsFilter;
+export default CardsFilter;
