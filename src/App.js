36d18b03eab751ef9ad6f6f69ac6683723133421
AmanNookat <amantur.mendibaev2003@gmail.com>
2024-01-06 06:46:03 +0600

    a

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import MainRoutes from "./routing/MainRoutes";
 import Navbar from "./components/ui/Navbar/Navbar";
 import { ToastContainer } from "react-toastify";
 
 const App = () => {
+  // test comment
+  const [a, setState] = useState(0);
+
   return (
     <>
       <Navbar />
